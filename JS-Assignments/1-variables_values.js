6a13c0542d4a793da7e0af1c525be182f35e79bc
KCReact <kalyanchepena95@gmail.com>
2026-04-06 14:10:57 +0530

    Create 1-variables_values.js

---
+++ JS-Assignments/1-variables_values.js
@@ -0,0 +1,12 @@
+// Values and Variables﻿
+// Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+
+// Log their values to the console.
+
+let country = "India";
+let continent = "Asia";
+let population = "1476";
+
+console.log(country);
+console.log(continent);
+console.log(population);
