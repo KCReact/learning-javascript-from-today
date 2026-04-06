e241a6ea0ed210afdff98059014d57b0daab9bb1
KCReact <kalyanchepena95@gmail.com>
2026-04-06 14:26:23 +0530

    Create 3-let_const_var.js

---
+++ JS-Assignments/3-let_const_var.js
@@ -0,0 +1,21 @@
+// let, const and var﻿
+// Set the value of language to the language spoken where you live (some countries have multiple languages, but just choose one).
+
+// Think about which variables should be const variables (which values will never change, and which might change?). Then, change these variables to const.
+
+// Try to change one of the changed variables now, and observe what happens.
+
+const isIsland = false;
+const country = "India";
+const continent = "Asia";
+let population = "1476";
+let language = "Hindi";
+
+console.log(isIsland);
+console.log(country);
+console.log(continent);
+console.log(population);
+
+// country = "China";
+// continent = "Europe";
+// Errpr : Assignment to constant variable.
