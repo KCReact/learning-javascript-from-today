2424cf80d55b789ab5f481086b29011fa93e9424
Kalyan <kalyanchepena95@gmail.com>
2026-04-06 18:10:42 +0530

    Create 10-Conditional_Ternary_Operator.js

---
+++ JS-Assignments/10-Conditional_Ternary_Operator.js
@@ -0,0 +1,11 @@
+// The Conditional (Ternary) Operator﻿
+// If your country's population is greater than 33 million, use the ternary operator to log a string like this to the console: "Portugal's population is above average". Otherwise, simply log "Portugal's population is below average". Notice how only one word change between these two sentences!
+
+// After checking the result, change the population temporarily to 13 and then to 130. See the different results, and set the population back to original.
+
+const population = 1476;
+
+// population = 13;
+// population = 130;
+
+console.log(population > 33 ? "India's population is above average" : " India's population is below average");
