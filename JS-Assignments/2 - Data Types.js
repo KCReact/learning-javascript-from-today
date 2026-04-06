4a62f32c1e4949b987ea134f61f90d79a1a31bb7
KCReact <kalyanchepena95@gmail.com>
2026-04-06 14:12:56 +0530

    Create 2 - Data Types.js

---
+++ JS-Assignments/2 - Data Types.js
@@ -0,0 +1,14 @@
+// Data Types﻿
+// Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
+
+// Log the types of isIsland, population, country and language to the console.
+
+let isIsland = false;
+let country = "India";
+let continent = "Asia";
+let population = "1476";
+
+console.log(isIsland);
+console.log(country);
+console.log(continent);
+console.log(population);
