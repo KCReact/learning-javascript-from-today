968c9c6cf682c56b4cc0d26ab604f263e0769e2f
KCReact <kalyanchepena95@gmail.com>
2026-04-06 16:18:21 +0530

    Create 6-Type_Conversion_Coercion.js

---
+++ JS-Assignments/6-Type_Conversion_Coercion.js
@@ -0,0 +1,15 @@
+// Type Conversion and Coercion﻿
+// Predict the result of these 5 operations without executing them:
+
+// console.log('9' - '5'); // -> ?
+// console.log('19' - '13' + '17'); // -> ?
+// console.log('19' - '13' + 17); // -> ?
+// console.log('123' < 57); // -> ?
+// console.log(5 + 6 + '4' + 9 - 4 - 2); // -> ?
+// Execute the operations to check if you were right.
+
+'4'
+'617'
+23
+false
+'1143'
