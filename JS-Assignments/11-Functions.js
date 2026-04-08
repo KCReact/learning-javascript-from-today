f76869453aad3d2e5b83f0a868445bb45bf21235
Kalyan <kalyanchepena95@gmail.com>
2026-04-08 21:59:32 +0530

    Create 11-Functions.js

---
+++ JS-Assignments/11-Functions.js
@@ -0,0 +1,14 @@
+// Functions﻿
+// Write a function called describeCountry which takes three parameters: country, population and capitalCity. Based on this input, the function returns a string with this format: 'Finland has 6 million people and its capital city is Helsinki'.
+
+// Call this function 3 times, with input data for 3 different countries. Store the returned values in 3 different variables, and log them to the console.
+
+function describeCountry(country, population, capitalCity) {
+    console.log(
+        `${country} has ${population} million people and it's capital city is ${capitalCity}`,
+    );
+}
+
+describeCountry("India", "1476", "New Delhi");
+describeCountry("China", "1450", "Beijing");
+describeCountry("Russia", "476", "Moscow");
