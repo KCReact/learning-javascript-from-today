31977158fa3fe1f2c20f3ba2511f42511a340e30
Kalyan <kalyanchepena95@gmail.com>
2026-04-08 22:10:36 +0530

    Create 13-Arrow Functions.js

---
+++ JS-Assignments/13-Arrow Functions.js
@@ -0,0 +1,8 @@
+// Arrow Functions﻿
+// Recreate the last assignment, but this time create an arrow function called percentageOfWorld3.
+
+const percentageOfWorld3 = (population) => (population / worldPopulation) * 100;
+const percIndia3 = percentageOfWorld3(1476);
+const percChina3 = percentageOfWorld3(1471);
+const percUSA3 = percentageOfWorld3(400);
+console.log(percIndia3, percChina3, percUSA3);
