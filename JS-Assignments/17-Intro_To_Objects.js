11d503bd6a2b20c60ea6ecc31ae8ca913507658f
Kalyan <kalyanchepena95@gmail.com>
2026-04-09 08:44:36 +0530

    Create 17-Intro_To_Objects.js

---
+++ JS-Assignments/17-Intro_To_Objects.js
@@ -0,0 +1,19 @@
+// Introduction to Objects﻿
+// Create an object called myCountry for a country of your choice, containing properties country, capital, language, population and neighbours (an array like we used in previous assignments).
+
+const myCountry = {
+  country: "India",
+  capital: "New Delhi",
+  population: 1476,
+  neighbours: [
+    "Nepal",
+    "Bhutan",
+    "Myanmmar",
+    "SriLanka",
+    "China",
+    "Bangladesh",
+    "Pakistan",
+  ],
+};
+
+console.log(myCountry);
