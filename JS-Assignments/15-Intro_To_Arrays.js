f5b7c41f039bdcd0c9f428e6e9731d5e69e47ae5
Kalyan <kalyanchepena95@gmail.com>
2026-04-09 08:06:37 +0530

    Create 15-Intro_To_Arrays.js

---
+++ JS-Assignments/15-Intro_To_Arrays.js
@@ -0,0 +1,26 @@
+// Introduction to Arrays﻿
+// Create an array containing 4 population values of 4 countries of your choice. You may use the values you have been using previously. Store this array into a variable called populations.
+
+// Log to the console whether the array has 4 elements or not (true or false).
+
+// Create an array called percentages containing the percentages of the world population for these 4 population values. Use the function percentageOfWorld1 that you created earlier to compute the 4 percentage values.
+
+
+const populations = [1476, 1441, 400, 250];
+
+console.log(populations.length === 4);
+
+const worldPopulation = 7900;
+
+function percentageOfWorld1(population) {
+  return (population / worldPopulation) * 100;
+}
+
+const percentages = [
+  percentageOfWorld1(populations[0]),
+  percentageOfWorld1(populations[1]),
+  percentageOfWorld1(populations[2]),
+  percentageOfWorld1(populations[3]),
+];
+
+console.log(percentages);
