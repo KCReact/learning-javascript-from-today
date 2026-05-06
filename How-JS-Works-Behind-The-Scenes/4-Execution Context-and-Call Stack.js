78325c4066f3b02753deff7f6b455517a883bf28
Kalyan <kalyanchepena95@gmail.com>
2026-05-06 06:05:40 +0530

    Update 4-Execution Context-and-Call Stack.js

--- How-JS-Works-Behind-The-Scenes/4-Execution Context-and-Call Stack.js
+++ How-JS-Works-Behind-The-Scenes/4-Execution Context-and-Call Stack.js
@@ -12,8 +12,8 @@
   3) this keyword.
 
 -> A Global Execution Context (GEC) is created first which contains all the variable declarations and code for functions.
--> Then as we encounter functions in the code, each function will have it's own Execution Context.
+-> Then as we encounter function calls in the code, each function would have it's own Execution Context created.
 -> In a program with 100s or 1000s of functions, these Execution Contexts are stacked on top of each other in something called "Call Stack".
--> The topmost execution context can be considered as the one which is in execution currently.
+-> The topmost execution context on the Call Stack can be considered as the one which is in execution currently.
 -> Once the execution reaches the end of function body, it's execution context is popped off the stack and the remaining Execution Contexts are executed in the order.
 -> Once all the functions are done with execution, finally GEC is also popped off and garbage collection process proceeds.
